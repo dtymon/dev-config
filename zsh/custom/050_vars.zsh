7736101338107424578b74be8f7c350c8126a73d
David Tymon <david.tymon@gmail.com>
2025-07-10 11:50:33 +1000

    Disable tmux window title changes

--- zsh/custom/050_vars.zsh
+++ zsh/custom/050_vars.zsh
@@ -2,3 +2,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 export JAVA_HOME="/opt/homebrew/opt/openjdk@11"
+
+# Emacs related
+export ESLINT_USE_FLAT_CONFIG=true
+export LSP_USE_PLISTS=true
