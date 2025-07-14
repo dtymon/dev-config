24dc5508836fd315ea67d35cab9ccb6ff8559c54
David Tymon <david.tymon@gmail.com>
2025-07-15 08:28:21 +1000

    Add hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,82 @@
+-- Pre-load some modules
+local timer = hs.timer
+local alert = hs.alert
+local mouse = hs.mouse
+local eventtap = hs.eventtap
+
+hs.application.enableSpotlightForNameSearches(true)
+
+function warpPointerToWindow(win)
+   local winFrame = win:frame()
+   x = winFrame.x + winFrame.w / 2
+   y = winFrame.y + winFrame.h / 2
+   hs.mouse.absolutePosition({ x = x, y = y })
+end
+
+function cycleAppWindows(app)
+   local windows = app:allWindows()
+   if #windows < 2 then
+      return
+   end
+
+   -- Get the windows into a deterministic order
+   table.sort(windows, function (a, b) return a:id() < b:id() end)
+
+   local currentWin = hs.window.focusedWindow()
+   local nextWin = windows[1]
+   for i, win in ipairs(windows) do
+      if win:id() == currentWin:id() then
+         local nextIndex = (i % #windows) + 1
+         nextWin = windows[nextIndex]
+         break
+      end
+   end
+
+   nextWin:focus()
+   hs.timer.doAfter(0.7, function()
+                       warpPointerToWindow(nextWin)
+   end)
+end
+
+function focusAndWarpPointerToApp(appName)
+   -- If it is already the focused application then cycle through its windows
+   local currentApp = hs.application.frontmostApplication()
+   if currentApp and currentApp:name() == appName then
+      cycleAppWindows(currentApp)
+      return
+   end
+
+   hs.application.launchOrFocus(appName)
+   hs.timer.doAfter(0.7, function()
+                       hs.alert.show(appName)
+                       local app = hs.application.get(appName)
+                       local win = app:mainWindow()
+                       if win then
+                          warpPointerToWindow(win)
+                       end
+   end)
+end
+
+hs.hotkey.bind({}, "padEnter", function ()
+      focusAndWarpPointerToApp("Emacs")
+end)
+
+hs.hotkey.bind({}, "pad+", function ()
+      focusAndWarpPointerToApp("WezTerm")
+end)
+
+hs.hotkey.bind({}, "pad3", function ()
+      focusAndWarpPointerToApp("Firefox")
+end)
+
+hs.hotkey.bind({}, "pad2", function ()
+      focusAndWarpPointerToApp("Slack")
+end)
+
+hs.hotkey.bind({}, "pad1", function ()
+      focusAndWarpPointerToApp("Microsoft Teams")
+end)
+
+hs.hotkey.bind({}, "pad-", function ()
+      focusAndWarpPointerToApp("Microsoft Outlook")
+end)
