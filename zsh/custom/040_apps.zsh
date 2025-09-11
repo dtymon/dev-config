9edfa648832732464d0aeeadc2a18ed84b359a6c
David Tymon <david.tymon@gmail.com>
2025-09-11 15:34:49 +1000

    Add VSCode to PATH if it is available

--- zsh/custom/040_apps.zsh
+++ zsh/custom/040_apps.zsh
@@ -3,4 +3,8 @@ export NVM_DIR="$HOME/.nvm"
 # nvm is now lazy loaded as it slows down shell creation too much
 # [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
 
+if [ -d "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" ]; then
+    path+=("/Applications/Visual Studio Code.app/Contents/Resources/app/bin")
+fi
+
 path+=("$HOMEBREW_PREFIX/opt/postgresql@15/bin")
