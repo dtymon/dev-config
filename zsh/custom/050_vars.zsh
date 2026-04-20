190e779f7e60635534b3ae8b1084237aa12b7274
David Tymon <david.tymon@gmail.com>
2026-04-20 15:29:35 +1000

    Move to podman as colima is really starting to suck

--- zsh/custom/050_vars.zsh
+++ zsh/custom/050_vars.zsh
@@ -1,5 +1,10 @@
 export HOMEBREW_NO_AUTO_UPDATE=1
-export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
+
+# colima is really starting to suck ass lately. Lets use podman which does not
+# need this to be set.
+# export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
+export PODMAN_COMPOSE_PROVIDER=podman-compose
+
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 export JAVA_HOME="/opt/homebrew/opt/openjdk@11"
 export NODE_DEFAULT_VERSION="20.19.3"
